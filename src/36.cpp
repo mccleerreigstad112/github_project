e58293e3e62f256838eced107c1f6114578ce68c
mccleerreigstad112 <mccleerreigstad112@gmail.com>
2025-04-26 05:20:39 +0000

    Add 36.cpp

---
+++ src/36.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    double b = 3.14;
+    char c = 'A';
+    long d = 0xabcdef; // 用 hexadecimal 编码
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
+
+    return 0;
+}
