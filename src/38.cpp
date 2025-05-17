04216304e9e8dadc2b382fbf75b808cdbd1f190c
mccleerreigstad112 <mccleerreigstad112@gmail.com>
2025-05-17 04:13:23 +0000

    Add 38.cpp

---
+++ src/38.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    bool is_student = true;
+    cout << "Age: " << age << ", Is Student: " << is_student << endl;
+    return 0;
+}
