e86fba6bd76349d6cce505c91a3790e262a5ec9d
mccleerreigstad112 <mccleerreigstad112@gmail.com>
2025-05-17 08:59:47 +0000

    Add 39.cpp

---
+++ src/39.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    std::cout << "Value of x: " << x << std::endl;
+
+    if (x > 0) {
+        std::cout << "x is greater than zero." << std::endl;
+    } else {
+        std::cout << "x is not greater than zero." << std::endl;
+    }
+
+    return 0;
+}
