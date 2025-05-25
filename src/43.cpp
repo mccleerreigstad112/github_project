0eddec1050470d1b28b7327ac70cc4d1fe5747a6
mccleerreigstad112 <mccleerreigstad112@gmail.com>
2025-05-25 00:23:07 +0000

    Add 43.cpp

---
+++ src/43.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    if (num1 > num2) {
+        std::cout << "num1 is greater than num2" << std::endl;
+    } else {
+        std::cout << "num2 is greater than num1" << std::endl;
+    }
+
+    return 0;
+}
