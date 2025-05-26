b3886adee82eb0a340b5294292003c2311d35216
mccleerreigstad112 <mccleerreigstad112@gmail.com>
2025-05-26 12:16:43 +0000

    Add 45.cpp

---
+++ src/45.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 20;
+    if (x > y) {
+        cout << "x is greater than y." << endl;
+    } else {
+        cout << "y is greater than x." << endl;
+    }
+}
